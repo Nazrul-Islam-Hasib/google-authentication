ab5d0980ab3d5997f2de2ca317c0828403a4feb2
Md. Nazrul Islam <md.-nazrul.islam@stud.uni-bamberg.de>
2022-07-28 10:34:39 +0200

    google auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+      //Google signin
+      const provider = new GoogleAuthProvider();
+      const handleGoogleSignIn = () => {
+          signInWithPopup(auth, provider)
+              .then((res) => {
+                  console.log(res.user)
+                  const user = res.user
+                  setUser(user);
+                  console.log(user);
+              }).catch((error) => {
+                  console.log(error);
+              });
+          }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Sign in with google</button>
+      <h2>{user.displayName}</h2>
+      <h4>{user.email}</h4>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3cTXMFkFVm7iv4ijl0R3FvF8UDmEVEjQ",
+    authDomain: "react-auth-3c321.firebaseapp.com",
+    projectId: "react-auth-3c321",
+    storageBucket: "react-auth-3c321.appspot.com",
+    messagingSenderId: "657031678482",
+    appId: "1:657031678482:web:b4e177121a08db1ab366cb"
+  };
+
+  export default firebaseConfig;
